60aa992ca7ccb767d1cd13135a2e5cde34a84549
Aprille P <aprillesakura@gmail.com>
2018-11-20 02:15:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  const new_array = []
+
+  for (let i = 0; i < musicians.length; i++) {
+    new_array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return new_array
+}
+
+function johnLennonFacts(facts) {
+  var exclamation = [];
+  let spot = 0;
+  while (spot < facts.length) {
+    let newString = facts[spot] + "!!!";
+    exclamation.push(newString);
+    spot++;
+  }
+
+  return exclamation;
+}
+
+function iLoveTheBeatles(number) {
+  var love = []
+  //var i = 0;
+
+  do {
+    love.push("I love the Beatles!")
+  } while (love < 15);
+
+  // function increment() {
+  //   i = i + 1;
+  // }
+
+  return love + "I love the Beatles!";
+}
